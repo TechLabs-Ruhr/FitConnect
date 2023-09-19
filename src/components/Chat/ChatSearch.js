373176b9fe3ac7b305ef35076233fae3042b9c88
Daniel33970 <daniel_danilov@web.de>
2023-09-19 10:59:39 +0200

    functions created

---
+++ src/components/Chat/ChatSearch.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ChatSearch = () => {
+  return (
+    <div>ChatSearch</div>
+  )
+}
---
+++ src/components/Chat/ChatSideBar.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ChatSideBar = () => {
+  return (
+    <div>ChatSideBar</div>
+  )
+}
