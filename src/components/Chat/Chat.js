df7269ae5bfec8fd93bf0186124cb2752712541b
Daniel33970 <daniel_danilov@web.de>
2023-09-19 13:39:07 +0200

    Messages and input addded

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import { Messages } from './Messages'
+import { Input } from './Input'
 
 export const Chat = () => {
   return (
-    <div className='chat-chat'>Chat</div>
+    <div className='chat-chat'>
+        <div className="chatInfo">
+            <span>Jane</span>
+            <div className="chatIcons">
+                
+            </div>
+            
+        </div>
+        <Messages/>
+        <Input/>
+    </div>
   )
 }
--- src/components/Chat/Chats.js
+++ src/components/Chat/Chats.js
@@ -2,6 +2,28 @@ import React from 'react'
 
 export const Chats = () => {
   return (
-    <div>Chats</div>
+    <div className='chats'>
+        <div className="userChat">
+            <img src="https://img.welt.de/img/sport/fitness/mobile246350898/8501626877-ci23x11-w1136/Athletic-man-doing-pushups-exercise-at-gym.jpg" alt="" />
+            <div className="userChatInfo">
+                <span>Jane</span>
+                <p>Hello</p>
+            </div>
+        </div>
+        <div className="userChat">
+            <img src="https://img.welt.de/img/sport/fitness/mobile246350898/8501626877-ci23x11-w1136/Athletic-man-doing-pushups-exercise-at-gym.jpg" alt="" />
+            <div className="userChatInfo">
+                <span>Jane</span>
+                <p>Hello</p>
+            </div>
+        </div>
+        <div className="userChat">
+            <img src="https://img.welt.de/img/sport/fitness/mobile246350898/8501626877-ci23x11-w1136/Athletic-man-doing-pushups-exercise-at-gym.jpg" alt="" />
+            <div className="userChatInfo">
+                <span>Jane</span>
+                <p>Hello</p>
+            </div>
+        </div>
+    </div>
   )
 }
