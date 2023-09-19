3459fb0aa7c0acb9732ff35de4f4810921f40dbf
Daniel33970 <daniel_danilov@web.de>
2023-09-19 14:54:00 +0200

    In settings if you hover over photo, there is the cursor pointer/ CSS Part of chat is ready

--- src/components/Chat/Input.js
+++ src/components/Chat/Input.js
@@ -2,6 +2,12 @@ import React from 'react'
 
 export const Input = () => {
   return (
-    <div className='input'>Input</div>
+    <div className='input'>
+        <input type="text" placeholder='Type something...'/>
+        <div className="send">
+            <img src="" alt="" />
+            <button>Send</button>
+        </div>
+    </div>
   )
 }
--- src/components/Chat/Message.js
+++ src/components/Chat/Message.js
@@ -2,6 +2,15 @@ import React from 'react'
 
 export const Message = () => {
   return (
-    <div>Message</div>
+    <div className='message owner'>
+        <div className="messageInfo">
+            <img src="https://img.welt.de/img/sport/fitness/mobile246350898/8501626877-ci23x11-w1136/Athletic-man-doing-pushups-exercise-at-gym.jpg" alt="" />
+            <span>just now</span>
+        </div>
+        <div className="messageContent">
+            <p>hello</p>
+            <img src="https://img.welt.de/img/sport/fitness/mobile246350898/8501626877-ci23x11-w1136/Athletic-man-doing-pushups-exercise-at-gym.jpg" alt="" />
+        </div>
+    </div>
   )
 }
