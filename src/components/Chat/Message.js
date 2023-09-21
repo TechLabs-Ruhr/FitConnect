76e3ba65cd0071bc09addf267d00a50ffee46697
Daniel33970 <daniel_danilov@web.de>
2023-09-21 19:29:19 +0200

    nearly done only error in message.js with senderid, text and img

--- src/components/Chat/Message.js
+++ src/components/Chat/Message.js
@@ -1,16 +1,39 @@
-import React from 'react'
+import React, { useContext, useEffect, useRef } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import { ChatContext } from "../../context/ChatContext";
 
-export const Message = () => {
+const Message = ({ message }) => {
+  const { currentUser } = useContext(AuthContext);
+  const { data } = useContext(ChatContext);
+
+  const ref = useRef();
+
+  useEffect(() => {
+    ref.current?.scrollIntoView({ behavior: "smooth" });
+  }, [message]);
+
   return (
-    <div className='message owner'>
-        <div className="messageInfo">
-            <img src="https://img.welt.de/img/sport/fitness/mobile246350898/8501626877-ci23x11-w1136/Athletic-man-doing-pushups-exercise-at-gym.jpg" alt="" />
-            <span>just now</span>
-        </div>
-        <div className="messageContent">
-            <p>hello</p>
-            <img src="https://img.welt.de/img/sport/fitness/mobile246350898/8501626877-ci23x11-w1136/Athletic-man-doing-pushups-exercise-at-gym.jpg" alt="" />
-        </div>
+    <div
+      ref={ref}
+      // className={`message ${message.senderId === currentUser.uid && "owner"}`}
+    >
+      <div className="messageInfo">
+        {/* <img
+          src={
+            message.senderId === currentUser.uid
+              ? currentUser.photoURL
+              : data.user.photoURL
+          }
+          alt=""
+        /> */}
+        <span>just now</span>
+      </div>
+      <div className="messageContent">
+        {/* <p>{message.text}</p>
+        {message.img && <img src={message.img} alt="" />} */}
+      </div>
     </div>
-  )
-}
+  );
+};
+
+export default Message;
--- src/components/Chat/Messages.js
+++ src/components/Chat/Messages.js
@@ -1,16 +1,34 @@
-import React from 'react'
-import { Message } from './Message'
+import React, { useContext, useEffect, useState } from 'react'
+import Message from './Message'
+import { ChatContext } from '../../context/ChatContext';
+import { onSnapshot, doc } from 'firebase/firestore';
+import {db} from "../../config/firebase"
 
 export const Messages = () => {
+  const [messages, setMessages] = useState([]);
+  const {data} = useContext(ChatContext);
+
+  useEffect(()=>{
+    const unSub = onSnapshot(doc(db,"chats",data.chatId), (doc)=>{
+      doc.exists() && setMessages(doc.data().messages)
+    })
+
+
+    return()=>{
+      unSub()
+    }
+  },[data.chatId])
+
+  console.log(messages)
+
+
+
   return (
     <div className='messages'>
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
+        {messages.map((m) => (
+        <Message messages={m} key={m.id}/>
+        ))}
     </div>
-  )
-}
+  );
+};
+
