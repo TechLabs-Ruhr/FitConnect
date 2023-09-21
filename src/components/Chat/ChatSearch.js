1f00c0c6ff2f22913e255ffbaf74c277cb49981a
Daniel33970 <daniel_danilov@web.de>
2023-09-21 13:51:32 +0200

    if you click on searched users and you havent clicked on him before, a new object is beeing created in chats

--- src/components/Chat/ChatSearch.js
+++ src/components/Chat/ChatSearch.js
@@ -32,48 +32,41 @@ export const ChatSearch = () => {
   };
 
   const handleSelect = async () => {
-    
-    // check wether the group(chats in firestore) exists, if not create
-    const combinedId = 
-      currentUser.uid > user.uid 
-       ? currentUser.uid + user.uid
-       : user.uid + currentUser.uid;
+    //check whether the group(chats in firestore) exists, if not create
+    const combinedId =
+      currentUser.uid > user.uid
+        ? currentUser.uid + user.uid
+        : user.uid + currentUser.uid;
+    try {
+      const res = await getDoc(doc(db, "chats", combinedId));
 
-       try{
-    const res = await getDoc(doc(db,"chats", combinedId));
-       
-
-    if(!res.exists()){
-      console.log("1");
+      if (!res.exists()) {
         //create a chat in chats collection
-        await setDoc(doc(db,"chats",combinedId),{messages: [] });
+        await setDoc(doc(db, "chats", combinedId), { messages: [] });
 
         //create user chats
-        await updateDoc(doc(db, "userChats", currentUser.uid),{
+        await updateDoc(doc(db, "userChats", currentUser.uid), {
           [combinedId + ".userInfo"]: {
             uid: user.uid,
             displayName: user.displayName,
             photoURL: user.photoURL,
           },
-          [combinedId+".date"]: serverTimestamp(),
+          [combinedId + ".date"]: serverTimestamp(),
         });
-
 
         await updateDoc(doc(db, "userChats", user.uid), {
           [combinedId + ".userInfo"]: {
-            uid:currentUser.uid,
+            uid: currentUser.uid,
             displayName: currentUser.displayName,
             photoURL: currentUser.photoURL,
           },
           [combinedId + ".date"]: serverTimestamp(),
         });
-    }
-  }catch (err) {}
-    // create user chats
+      }
+    } catch (err) {}
 
     setUser(null);
     setUsername("")
-
   };
   return (
     <div className='chatsearch'>
